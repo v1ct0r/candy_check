fac356c2b72dff0a64b44f0430e105b577e6ec50
Victor Mokry <viktor.mokriy@gera-it.com>
2021-11-07 23:19:47 +0200

    read ios subscription cancelled information

--- lib/candy_check/app_store/receipt_collection.rb
+++ lib/candy_check/app_store/receipt_collection.rb
@@ -2,17 +2,25 @@ module CandyCheck
   module AppStore
     # Store multiple {Receipt}s in order to perform collective operation on them
     class ReceiptCollection
+
+      PENDING_RENEWAL_INFO_KEYS = %w(expiration_intent
+                                     is_in_billing_retry_period
+                                     auto_renew_status
+                                     auto_renew_product_id).freeze
+
       # Multiple receipts as in verfication response
       # @return [Array<Receipt>]
       attr_reader :receipts
+      attr_reader :pending_renewal_info
 
       # Initializes a new instance which bases on a JSON result
       # from Apple's verification server
       # @param attributes [Array<Hash>] raw data from Apple's server
-      def initialize(attributes)
+      def initialize(attributes, pending_renewal_info)
         @receipts = attributes.map {|r| Receipt.new(r) }.sort{ |a, b|
           a.purchase_date - b.purchase_date
         }
+        @pending_renewal_info = pending_renewal_info
       end
 
       # Check if the latest expiration date is passed
@@ -37,6 +45,13 @@ module CandyCheck
       # @return [Integer]
       def overdue_days
         (Date.today - expires_at.to_date).to_i
+      end
+
+      def cancelled?
+        it last_info = @pending_renewal_info&.last
+          return last_info.key?('auto_renew_status') && last_info['auto_renew_status'] == '0'
+        end
+        return false
       end
     end
   end
